ae9b6411e0d9a622624d144730a19dafe78b5b24
mariateleki <mariatelekiresearch@gmail.com>
2026-04-04 13:34:43 -0500

    update w/ demographics

---
+++ docs/demographics.js
@@ -0,0 +1,37 @@
+const DEMOGRAPHICS = [
+  {
+    id: "demo_age",
+    type: "multiple_choice",
+    text: "What is your age range?",
+    options: ["18-24", "25-34", "35-44", "45-54", "55-64", "65+"]
+  },
+  {
+    id: "demo_gender",
+    type: "multiple_choice",
+    text: "What is your gender?",
+    options: ["Male", "Female", "Non-binary", "Prefer not to say"]
+  },
+  {
+    id: "demo_education",
+    type: "multiple_choice",
+    text: "What is your highest level of education?",
+    options: ["High school or less", "Some college", "Bachelor's degree", "Master's degree", "Doctoral or professional degree"]
+  },
+  {
+    id: "demo_english",
+    type: "multiple_choice",
+    text: "Is English your first language?",
+    options: ["Yes", "No"]
+  },
+  {
+    id: "demo_transcription_exp",
+    type: "multiple_choice",
+    text: "How much experience do you have reading or working with transcripts?",
+    options: ["None", "A little", "Some", "A lot", "It's part of my job"]
+  },
+  {
+    id: "demo_industry",
+    type: "free_response",
+    text: "What industry or field do you work in?"
+  }
+];
